1f26b786fa986826631bb83e44d30cb594e646f9
Fabio Hernandez <f_a_b_io_@hotmail.com>
2022-11-02 16:37:31 -0500

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/fabioHernandezRu/hello-golang
+
+go 1.16
---
+++ utils.go
@@ -0,0 +1,7 @@
+package utils
+
+import "fmt"
+
+func HelloWorld() { //primera letra en mayuscula para decir que es pública
+	fmt.Println("Hello World from my repo")
+}
